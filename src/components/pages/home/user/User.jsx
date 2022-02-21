2ea927c9344c9b2950cc981a5ee8c97584f57ada
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2022-02-21 11:20:17 +0530

    -

--- src/components/pages/home/user/User.jsx
+++ src/components/pages/home/user/User.jsx
@@ -7,12 +7,15 @@ import {
   LocationSearching,
   Publish,
 } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 export default function User() {
   return (
     <div className="user">
       <div className="userTitleContainer">
         <h1 className="userTitle">Edit User</h1>
-        <button className="userAddButton">Create</button>
+        <Link to="/newuser">
+          <button className="userAddButton">Create</button>
+        </Link>
       </div>
       <div className="userContainer">
         <div className="userShow">
